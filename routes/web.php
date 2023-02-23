ca5300946bfa4880318ffd0cf796de1f764fbdbe
Your Name <almannofrancesco@gmail.com>
2023-02-23 17:57:46 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController as ComicController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+// RECUPERO I FUMETTI
+Route::get('/', 'ComicController@index')->name('homepage');
+
+Route::get('/fumetti', function () {
+    $comics = config('db');
+
+    return view('fumetti', compact('comics'));
+})->name('comics');
+
+Route::get('/fumetto/{titolo}', function ($titolo) {
+    $comics = config('db');
+});
